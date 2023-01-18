82f0584bfe45af97953a8e85909f808c428abe9e
Bernardo <bernardo.druziki@syxglobal.com>
2023-01-18 17:44:04 -0300

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;			
+public class Program
+{
+	public static void Main()
+	{
+		var urlSite = @"https://html-agility-pack.net/";//Url do site pro crawler varrer.
+
+        HtmlWeb htmlSite = new HtmlWeb();//pega o HTML pelo HTTP.
+
+        var htmlLoad = htmlSite.Load(urlSite);//Carrega o documento html.
+
+        var crawlerAction = htmlLoad.DocumentNode.SelectSingleNode("//body");//DocumentNode - pega o 'node' inteiro //selectSingleNode - Seleciona o primeiro 'node' atribuido .
+
+        Console.WriteLine(" Action: " + crawlerAction.Name + "\n" + crawlerAction.OuterHtml);//outerHtml- obtém o HTML do 'node', com tags de abertura e fechamento.
+    }
+}
